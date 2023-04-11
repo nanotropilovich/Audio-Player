1e0fab3708fea9f9ae9abc0400ef0d4b1b58e44d
Ilya <igerasimovv2@gmail.com>
2023-04-11 06:14:40 +0300

    work +-

--- player/Views/AudioPlayerView.swift
+++ player/Views/AudioPlayerView.swift
@@ -37,32 +37,8 @@ struct AudioPlayerView: View {
         )
     var body: some View {
         VStack {
-            Button(action: {
-                withAnimation {
-                    if self.audioPlayer.isPlaying {
-                        self.audioPlayer.stopPlaying(at: self.currentTime)
-                    } else {
-                        if let url = self.fileImporter.selectedFileURL {
-                            self.audioPlayer.playAudio(from: url, startTime: self.currentTime)
-                        }
-                    }
-                    self.isPlaying.toggle()
-                }
-            }) {
-                Image(systemName: self.isPlaying ? "stop.circle" : "play.circle")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-            }
-            Slider(value: Binding(get: { self.currentTime }, set: { self.audioPlayer.seek(to: $0) }), in: 0...self.totalTime) {
-                Text("")
-            }
-            .onReceive(self.audioPlayer.$currentTime) { currentTime in
-                self.currentTime = currentTime ?? 0
-            }
-            .onReceive(self.audioPlayer.$duration) { duration in
-                self.totalTime = duration ?? 0
-            }
-            .disabled(self.audioPlayer.duration == nil)
+          
+          
             Button(action: {
                            store.removeAll()
                        }) {
@@ -101,43 +77,56 @@ struct AudioPlayerView: View {
 struct URLListView: View {
     @ObservedObject var store: URLStore
     @Binding var currentTime: TimeInterval
+    @State private var currentURLIndex: Int = 0
+    
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.urls, id: \.self) { url in
-                    NavigationLink(destination: URLDetailView(url: url, currentTime: $currentTime)) {
+                    NavigationLink(destination: URLDetailView(urls: store.urls, currentURLIndex: $currentURLIndex, currentTime: $currentTime)) {
                         Text("\(url.lastPathComponent)")
                     }
                 }
-                .onDelete(perform: delete)
-            }
-            .navigationTitle("Track List")
-            .navigationBarItems(trailing: EditButton())
-        }
-    }
-    
+                    .onDelete(perform: delete)
+                    }
+                    .navigationTitle("Track List")
+                    .navigationBarItems(trailing: EditButton())
+                    }
+                    }
     func delete(at offsets: IndexSet) {
         store.urls.remove(atOffsets: offsets)
     }
-}
+    }
 
 
 struct URLDetailView: View {
-    let url: URL
+    let urls: [URL]
+    @Binding var currentURLIndex: Int
     @State private var totalTime: TimeInterval = 0
     @State private var isPlaying = false
     @ObservedObject var audioPlayer = AudioPlayer()
     @Binding var currentTime: TimeInterval
+    
     var body: some View {
-        // Используем url в этом View
+        let url = urls[currentURLIndex]
+        
+        Slider(value: Binding(get: { self.currentTime }, set: { self.audioPlayer.seek(to: $0) }), in: 0...self.totalTime) {
+            Text("")
+        }
+        .onReceive(self.audioPlayer.$currentTime) { currentTime in
+            self.currentTime = currentTime ?? 0
+        }
+        .onReceive(self.audioPlayer.$duration) { duration in
+            self.totalTime = duration ?? 0
+        }
+        .disabled(self.audioPlayer.duration == nil)
+        
         Button(action: {
             withAnimation {
                 if self.audioPlayer.isPlaying {
                     self.audioPlayer.stopPlaying(at: self.currentTime)
                 } else {
-                    
-                        self.audioPlayer.playAudio(from: url, startTime: self.currentTime)
-                    
+                    self.audioPlayer.playAudio(from: url, startTime: self.currentTime)
                 }
                 self.isPlaying.toggle()
             }
@@ -146,12 +135,36 @@ struct URLDetailView: View {
                 .resizable()
                 .frame(width: 50, height: 50)
         }
-        Text("Selected URL: \(url.absoluteString)")
         
+        HStack {
+            Button(action: {
+                if currentURLIndex > 0 {
+                    currentURLIndex -= 1
+                }
+            }) {
+                Image(systemName: "backward.end.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
+            .disabled(currentURLIndex == 0)
+            
+            Spacer()
+            
+            Button(action: {
+                if currentURLIndex < urls.count - 1 {
+                    currentURLIndex += 1
+                }
+            }) {
+                Image(systemName: "forward.end.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
+            .disabled(currentURLIndex == urls.count - 1)
+        }
+        
+        Text("Selected URL: \(url.absoluteString)")
     }
 }
-
-
 
 
 
