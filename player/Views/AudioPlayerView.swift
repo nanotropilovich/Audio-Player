e18ccbe94bc5d72a8a2ab486bb3f277db69ced34
Ilya <igerasimovv2@gmail.com>
2023-04-12 02:47:25 +0300

    up

--- player/Views/AudioPlayerView.swift
+++ player/Views/AudioPlayerView.swift
@@ -96,10 +96,10 @@ struct URLListView: View {
             List {
                 ForEach(store.urls, id: \.self) { url in
                     NavigationLink(destination: URLDetailView(urls: store.urls, currentURLIndex: $currentURLIndex)) {
-                        Text("\(url.artist ?? "no name") - \(url.name ?? "no name")")
+                        Text("\(url.artist ?? "no name") - \(url.name)")
 
                        
-
+                        
                        
                         
                     }
@@ -133,61 +133,70 @@ struct URLDetailView: View {
     var body: some View {
         let url = urls[currentURLIndex]
         let asset = AVURLAsset(url: url.url)
-        Slider(value: Binding(get: { url.currentTime }, set: { self.audioPlayer.seek(to: $0) }), in: 0...self.totalTime) {
+        VStack {
+            Text("ya ded")
             Text("\(formattedTime(url.currentTime)) / \(formattedTime(self.totalTime))")
-            
-                }
-        
-                .onReceive(self.audioPlayer.$currentTime) { currentTime in
-                   
-                    url.currentTime = currentTime ?? 0
-                }
-        .onReceive(self.audioPlayer.$duration) { duration in
-            self.totalTime = duration ?? 0
-        }
-        .disabled(self.audioPlayer.duration == nil)
-        
-        Button(action: {
-            withAnimation {
-                if self.audioPlayer.isPlaying {
-                    self.audioPlayer.stopPlaying(at: url.currentTime)
-                } else {
-                    self.audioPlayer.playAudio(from: url.url, startTime: url.currentTime)
-                }
-                self.isPlaying.toggle()
+            Slider(value: Binding(get: { url.currentTime }, set: { self.audioPlayer.seek(to: $0) }), in: 0...self.totalTime) {
+              
+               
             }
-        }) {
-            Image(systemName: self.isPlaying ? "stop.circle" : "play.circle")
-                .resizable()
-                .frame(width: 50, height: 50)
-        }
-        
-        HStack {
-            Button(action: {
-                if currentURLIndex > 0 {
-                    currentURLIndex -= 1
-                }
-            }) {
-                Image(systemName: "backward.end.fill")
-                    .resizable()
-                    .frame(width: 20, height: 20)
+            .onReceive(self.audioPlayer.$currentTime) { currentTime in
+                url.currentTime = currentTime ?? 0
             }
-            .disabled(currentURLIndex == 0)
-            
-            Spacer()
+            .onReceive(self.audioPlayer.$duration) { duration in
+                self.totalTime = duration ?? 0
+            }
+            .disabled(self.audioPlayer.duration == nil)
             
             Button(action: {
-                if currentURLIndex < urls.count - 1 {
-                    currentURLIndex += 1
+                withAnimation {
+                    if self.audioPlayer.isPlaying {
+                        self.audioPlayer.stopPlaying(at: url.currentTime)
+                    } else {
+                        self.audioPlayer.playAudio(from: url.url, startTime: url.currentTime)
+                    }
+                    self.isPlaying.toggle()
                 }
             }) {
-                Image(systemName: "forward.end.fill")
+                Image(systemName: self.isPlaying ? "stop.circle" : "play.circle")
                     .resizable()
-                    .frame(width: 20, height: 20)
+                    .frame(width: 50, height: 50)
             }
-            .disabled(currentURLIndex == urls.count - 1)
+            
+            HStack {
+                Button(action: {
+                    if currentURLIndex > 0 {
+                        currentURLIndex -= 1
+                    }
+                }) {
+                    Image(systemName: "backward.end.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                }
+                .disabled(currentURLIndex == 0)
+                
+                Spacer()
+                
+                Button(action: {
+                    if currentURLIndex < urls.count - 1 {
+                        if self.audioPlayer.isPlaying {
+                                       self.audioPlayer.stopPlaying(at: url.currentTime)
+                            self.isPlaying.toggle()
+                                   }
+                        currentURLIndex += 1
+                    }
+                }) {
+                    Image(systemName: "forward.end.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                }
+                .disabled(currentURLIndex == urls.count - 1)
+            }
+            
+            Text("Selected URL: \(url.url.absoluteString)")
         }
-        
+        .frame(width: 300, height: 200) // здесь можно установить нужный размер для VStack
+
         Text("Album: \(url.album ?? "no album name")")
         Text("Duration: \(url.duration?.description ?? "unknown")")
        let minutes = Int((url.duration ?? 0) / 60)
